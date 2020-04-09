f379e5ec8ce07a7b188d3c373f435b7214d0f64c
zyycoder <zhangyycoder@gmail.com>
2020-04-10 00:08:07 +0800

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,14 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
+      name: `张洋洋`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://zyycode.github.com/Note`,
     social: {
-      twitter: `kylemathews`,
+      twitter: '2020Yang',
+      github: 'zyycode'
     },
   },
   plugins: [
